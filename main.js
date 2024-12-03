0bc6bb8f5282c8ce22dd576431e77db0ed5461ca
kuntal545 <kuntalbej@gmail.com>
2024-12-03 13:36:57 +0530

    Changes js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 
 
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
-
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
